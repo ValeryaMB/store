c31652cb7d7533772496e4732135f47093e7dca3
Valeria <yokoyamamoto92@gmail.com>
2023-01-15 22:39:32 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ComprasComponent } from './components/compras/compras.component';
+import { PagoComponent } from './components/pago/pago.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:LoginComponent
+  },
+  {
+    path:'2',component:ErrorComponent
+  },
+  {
+    path:'3',component:ComprasComponent
+  },
+  {
+    path:'4',component:PagoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
